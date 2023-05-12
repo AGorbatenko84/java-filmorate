183b3dd8a8b3f466a238e134f63a855952b9c20a
AGorbatenko84 <68078642+AGorbatenko84@users.noreply.github.com>
2023-05-12 23:08:10 +0300

    Update UserService.java

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -9,7 +9,6 @@ import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
